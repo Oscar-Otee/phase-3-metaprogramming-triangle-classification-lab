8a2be3362c6cacd266f05dd1bcf60670f411fc04
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-17 14:39:05 +0300

    Assignment completed successfully

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+# class Triangle
+#   # write code here
+# end
+
+
 class Triangle
-  # write code here
+  #attr_accessor :partner, :name
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if (@side1 == @side2 && @side2 == @side3 && @side1 == 0) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side2 == @side3 || @side1 == @side3 || @side1 == @side2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+# class TriangleError < StandardError
+# end
+
+# beyonce = Person.new("Beyonce")
+# beyonce.get_married("Jay-Z")
+# puts beyonce.name
